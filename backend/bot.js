61e4c51d026d09814761b977408a30f9f6eb1945
qalandarsarkar <qalandarsarkar42@gmail.com>
2026-05-28 20:20:08 +0500

    Update bot.js

--- backend/bot.js
+++ backend/bot.js
@@ -1,32 +1,28 @@
 require('dotenv').config();
-
 const TelegramBot = require('node-telegram-bot-api');
 
 const token = process.env.BOT_TOKEN;
 
-const bot = new TelegramBot(token, {
-  polling: true
-});
+// Polling ko hatayein ya sirf tabhi use karein jab local machine par ho
+const bot = new TelegramBot(token, { polling: process.env.NODE_ENV !== 'production' });
 
 bot.onText(/\/start(?: (.+))?/, async (msg, match) => {
-
   const referral = match[1];
 
-  await bot.sendMessage(msg.chat.id,
-    'Welcome To TON Tap Game',
-    {
+  try {
+    await bot.sendMessage(msg.chat.id, 'Welcome To TON Tap Game', {
       reply_markup: {
         inline_keyboard: [[
           {
             text: 'Open Game',
-            web_app: {
-              url: process.env.WEBAPP_URL
-            }
+            web_app: { url: process.env.WEBAPP_URL }
           }
         ]]
       }
-    }
-  );
+    });
+  } catch (error) {
+    console.error('Error sending message:', error);
+  }
 
   if (referral) {
     console.log('Referral:', referral);
